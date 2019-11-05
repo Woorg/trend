795968e7bfcec0f8adec89f1605ac6032dd64eea
Igor Gorlov <gorlov35@gmail.com>
2019-11-05 20:02:36 +0300

    add pages index, projects, guarantee, about-us, contacts

--- markup/assets/js/main.js
+++ markup/assets/js/main.js
@@ -42,7 +42,6 @@ import svg4everybody from 'svg4everybody';
     elements_selector: '.lazy',
     load_delay: 50,
     use_native: true
-    // ... more custom settings?
   });
 
 
@@ -81,19 +80,20 @@ import svg4everybody from 'svg4everybody';
     Slider
     */
 
-    const $clientsCarousel = $('.clients__carousel');
+    const $projectsSlider = $('.projects__slider');
 
-    if( $clientsCarousel.length > 0 ) {
-      $clientsCarousel.slick({
+    if( $projectsSlider.length > 0 ) {
+      $projectsSlider.slick({
         mobileFirst: true,
-        dots: true,
-        arrows: false,
+        dots: false,
+        arrows: true,
         // loop: false,
         lazyLoad: 'ondemand',
         infinite: false,
         slidesToShow: 1,
         slidesToScroll: 1,
         speed: 600,
+        fade: true,
         // variableWidth: true,
         adaptiveHeight: true,
         waitForAnimate: true,
@@ -108,11 +108,46 @@ import svg4everybody from 'svg4everybody';
         {
           breakpoint: 992,
           settings: {
-            slidesToShow: 2,
+            slidesToShow: 1,
           }
         }]
       });
     }
+
+
+    /* 
+      Callback
+    */
+
+    const $callbackTrigger = $('.open-popup');
+
+    $callbackTrigger.magnificPopup({
+      type:'inline',
+      midClick: true 
+    });
+
+
+
+    function pageWidget(pages) {
+      var widgetWrap = $('<div class="widget_wrap"><ul class="widget_list"></ul></div>');
+      widgetWrap.prependTo("body");
+      for (var i = 0; i < pages.length; i++) {
+        $('<li class="widget_item"><a class="widget_link" href="' + pages[i] + '.html' + '">' + pages[i] + '</a></li>').appendTo('.widget_list');
+      }
+      var widgetStilization = $('<style>body {position:relative} .widget_wrap{position:absolute;top:0;left:0;z-index:9999;padding:10px 20px;background:#222;border-bottom-right-radius:10px;-webkit-transition:all .3s ease;transition:all .3s ease;-webkit-transform:translate(-100%,0);-ms-transform:translate(-100%,0);transform:translate(-100%,0)}.widget_wrap:after{content:" ";position:absolute;top:0;left:100%;width:24px;height:24px;background:#222 url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQAgMAAABinRfyAAAABGdBTUEAALGPC/xhBQAAAAxQTFRF////////AAAA////BQBkwgAAAAN0Uk5TxMMAjAd+zwAAACNJREFUCNdjqP///y/DfyBg+LVq1Xoo8W8/CkFYAmwA0Kg/AFcANT5fe7l4AAAAAElFTkSuQmCC) no-repeat 50% 50%;cursor:pointer}.widget_wrap:hover{-webkit-transform:translate(0,0);-ms-transform:translate(0,0);transform:translate(0,0)}.widget_item{padding:0 0 10px}.widget_link{color:#fff;text-decoration:none;font-size:15px;}.widget_link:hover{text-decoration:underline} </style>');
+      widgetStilization.prependTo(".widget_wrap");
+    }
+  
+    pageWidget([
+      'index',
+      'projects',
+      'guarantees',
+      'about-us',
+      'contacts',
+
+
+    ]);
+
 
 
   });
